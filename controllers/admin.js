4125541afe0ac9aac9555f416b95421db1a5a2d2
NPB98 <nishibiswasroy@gmail.com>
2023-02-16 13:43:22 +0530

    Expense Tracker Project Newly Added Task

--- controllers/admin.js
+++ controllers/admin.js
@@ -3,7 +3,8 @@ const User=require('../models/user');
 const AWS=require('aws-sdk');
 const UserServices=require('../services/userServices');
 const S3Services=require('../services/S3services.js');
-const DownloadedFiles=require('../models/downloadedFiles')
+const DownloadedFiles=require('../models/downloadedFiles');
+const sequelize=require('../util/database');
 
 const getExpenses=async(req,res,next)=>{
     //Expense.findAll({where:{userId:req.user.id}})
@@ -12,6 +13,7 @@ const getExpenses=async(req,res,next)=>{
     //    res.status(200).json(expenses);
     // })
     // .catch(err => console.log(err));
+    const t=await sequelize.transaction();
     try{
      console.log('Request',req.query);
       const page = req.query.page || 1;
@@ -24,6 +26,7 @@ const getExpenses=async(req,res,next)=>{
       limit: Number(expensesPerPage),
     });
     //console.log(expenses);
+    await t.commit();
       res.status(201).json({
          expenses:expenses,
          currentPage: Number(page),
@@ -35,56 +38,74 @@ const getExpenses=async(req,res,next)=>{
       }); 
    }
   catch(err){
+      await t.rollback();
       res.status(404).json(err);
   }
  };
- const deleteExpense = (req,res,next)=>{
-   console.log(req);
+ const deleteExpense =async (req,res,next)=>{
+  const t=await sequelize.transaction();
+   //console.log(req);
     const id = req.params.expenseId;
     //console.log(id)
     Expense.findByPk(id)
-    .then(expense => {
-      //console.log(user);
-      return expense.destroy();
-    })
-    .then(response => {
+    .then(async(expense) => {
+     //  console.log(expense);
+      console.log(expense.userId);
+      const destroyed =await expense.destroy();
+      User.findByPk(expense.userId).then((expenses)=>{
+        console.log(expenses);
+        const totalExpen=expenses.dataValues.totalExpenses;
+        console.log(totalExpen);
+        const totalExpense=Number(totalExpen)-Number(expense.dataValues.amount);
+        User.update({
+          totalExpenses: totalExpense,
+        },{where:{id:expense.userId},transaction:t}).then(()=>{
+          t.commit();
         res.status(202).json('Successfully Deleted');
+        })
+      })
+      //const totalExpense=Number(req.user.totalExpenses)-Number(req.dataValues.amount);
     })
+    // .then(response => {
+    //     t.commit();
+    //     res.status(202).json('Successfully Deleted');
+    //   })
     .catch((err)=>{
-        console.log(err)
+      t.rollback();
+        console.log(err);
+        res.status(500).json({message:"Failed",success:false});
     })
  };
 
- const addExpense = (req,res,next)=>{
+ const addExpense = async(req,res,next)=>{
+  const t=await sequelize.transaction();
+  try{
     const amount = req.body.amount;
     const description = req.body.description;
     const category = req.body.category;
     console.log(req.user.id);
-    Expense.create({
+    const response=await Expense.create({
      amount:amount,
      description:description,
      category:category,
      userId:req.user.id
-  })
-  .then((response)=>{
+  },{transaction:t})
     const totalExpense=Number(req.user.totalExpenses)+Number(amount);
     console.log(totalExpense);
-    User.update({
+    await User.update({
       totalExpenses: totalExpense,
-    },{where:{id:req.user.id}})
-    .then(()=>{
-      res.status(201).json({expense:response})
-    })
-    .catch((err)=>{
-      return res.status(500).json({success:false,error:err})
-    })
-  })
-  .catch((err)=> {
+    },{where:{id:req.user.id},transaction:t})
+      await t.commit();
+      res.status(201).json({expense:response});
+  }
+  catch(err) {
+    await t.rollback();
     return res.status(500).json({success:false,error:err})
-  });
+  }
  };
 
  const downloadExpenses = async (req,res,next)=>{
+  const t=await sequelize.transaction();
   try{
   const expenses=await UserServices.getExpenses(req);
     const stringifiedExpenses=JSON.stringify(expenses);
@@ -96,20 +117,30 @@ const getExpenses=async(req,res,next)=>{
       download:fileUrl,
       userId:req.user.id
     })
+    await t.commit();
     res.status(200).json({fileUrl,success:true});
   }
   catch(err){
+    await t.rollback();
     console.log(err)
     res.status(500).json({fileUrl:'',success:false,err:err})
   }
  }
 
  const getDownloads=async(req,res,next)=>{
-  //console.log('DOWNLOADS',req.user.id);
-  DownloadedFiles.findAll({where:{userId:req.user.id}}).then((downloads)=>{
-    //console.log('DOWNLOADS',downloads);
-    res.status(200).json(downloads);
-  }).catch((err)=>console.log(err))
+  const t=await sequelize.transaction();
+  try{
+    //console.log('DOWNLOADS',req.user.id);
+  const downloads=DownloadedFiles.findAll({where:{userId:req.user.id}})
+  //console.log('DOWNLOADS',downloads);
+  await t.commit();
+  res.status(200).json(downloads);
+  }
+  catch(err){
+    await t.rollback();
+    console.log(err);
+  }
+  
   //console.log(req.user);
   // const downloads = await req.user.getDownloadedFiles();
   //     res.status(200).json(downloads);
